46353acbece5837872626e980c2b7d30c69347d2
BURDOTATTOO <116234944+BURDOTATTOO@users.noreply.github.com>
2024-04-27 14:07:30 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,3 +43,39 @@ $(document).ready(function(){
 		} setTimeout(showAll,400);
 	});
 });
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+	//responsive menu toggle
+	$("#menutoggle").click(function() {
+	  $('.xs-menu').toggleClass('displaynone');
+
+	  });
+	//add active class on menu
+	$('ul li').click(function() {
+	  e.preventDefault();
+	  $('li').removeClass('active');
+	  $(this).addClass('active');
+	});
+  //drop down menu  
+	  $(".drop-down").hover(function() {
+		$('.mega-menu').addClass('display-on');
+	  });
+	  $(".drop-down").mouseleave(function() {
+		$('.mega-menu').removeClass('display-on');
+	  });
+  
+  });
+
+
+
+
+
